3980785f1ddc0ad3bbc104c78626714be5722e43
bhavisha550 <bhavishamoghe@gmail.com>
2024-06-24 14:09:36 +0530

    initial commit

---
+++ src/components/frog/frog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frog',
+  standalone: true,
+  imports: [],
+  templateUrl: './frog.component.html',
+  styleUrl: './frog.component.css'
+})
+export class FrogComponent {
+
+}
