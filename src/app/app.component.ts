ce7162264d09b421af1e91bf47a5e3c8ec91dfd0
bhavisha550 <bhavishamoghe@gmail.com>
2024-07-05 13:10:41 +0530

    form changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,91 +15,91 @@ import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , TodoListComponent, LoginComponent,UserListComponent, CommonModule, HeaderComponent],
+  imports: [RouterOutlet , TodoListComponent, LoginComponent,UserListComponent, CommonModule, HeaderComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   
 
-  // title = 'my-app';
-  // displayVal='';
-  // // events
-  // getName(val:any) {
-  //   console.warn(val)
-  //   this.displayVal=val
-  // }
-  // getData(val:string)
-  // {
-  //   console.warn(val)
-  // }
+  title = 'my-app';
+  displayVal='';
+  // events
+  getName(val:any) {
+    console.warn(val)
+    this.displayVal=val
+  }
+  getData(val:string)
+  {
+    console.warn(val)
+  }
 
-  // //counter
-  // title2 = 'counter';
-  // count = 0
-  // counter(type:string)
-  // {
-  //   type==='plus' ? this.count++ : this.count--
+  //counter
+  title2 = 'counter';
+  count = 0
+  counter(type:string)
+  {
+    type==='plus' ? this.count++ : this.count--
     
-  // }
+  }
 
-  // //style
-  // title3 = 'styling'
-
-
-  // //property binding
-  // title4 = 'property binding'
-  // name = "joy"
-  // disable=false;
-
-  // //if-else
-  // title5 = 'if-else'
-  // show=true
-
-  // //multiple conditions
-  // title6 = 'else-if'
-  // color="red";
-  // //color="green";
-
-  // //switch-case
-  // title7= 'switch-case'
-  // color1 = "green"
-
-  // //for loop
-  // title8 = 'for loop'
-  // users = ['isha', 'abc', 'xyz', 'sam', 'joy']
-  // userDetails=[
-  //   {name:'isha',email:'isha@some.com', phone:'27238729'},
-  //   {name:'abc',email:'abc@some.com', phone:'73782929'},
-  //   {name:'xyz',email:'xyz@some.com', phone:'67354829'},
-  //   {name:'sam',email:'sam@some.com', phone:'34257729'},
-  //   {name:'joy',email:'joy@some.com', phone:'60238729'}
-  // ];
+  //style
+  title3 = 'styling'
 
 
-  // //nested for loop
-  // title9 = 'nested for loop'
-  // users1=[
-  //     {name:'isha',email:'isha@some.com', phone:'27238729', social:['facebook','insta', 'snap']},
-  //     {name:'abc',email:'abc@some.com', phone:'73782929', social:['insta', 'snap']},
-  //     {name:'xyz',email:'xyz@some.com', phone:'67354829', social:['facebook', 'snap']},
-  //     {name:'sam',email:'sam@some.com', phone:'34257729', social:['facebook','insta', ]},
-  //     {name:'joy',email:'joy@some.com', phone:'60238729', social:['facebook','insta', 'snap', 'linkedin']}
-  // ];
+  //property binding
+  title4 = 'property binding'
+  name = "joy"
+  disable=false;
 
-  // //style binding
-  // title10 = 'style binding';
-  // color2 = "orange"
-  // bgcolor="red"
+  //if-else
+  title5 = 'if-else'
+  show=true
 
-  // updateColor()
-  // {
-  //   this.color2="pink"
-  //   this.bgcolor="skyblue"
-  // }
+  //multiple conditions
+  title6 = 'else-if'
+  color="red";
+  //color="green";
 
-  // //heading
-  // title11 = 'header component'
+  //switch-case
+  title7= 'switch-case'
+  color1 = "green"
+
+  //for loop
+  title8 = 'for loop'
+  users = ['isha', 'abc', 'xyz', 'sam', 'joy']
+  userDetails=[
+    {name:'isha',email:'isha@some.com', phone:'27238729'},
+    {name:'abc',email:'abc@some.com', phone:'73782929'},
+    {name:'xyz',email:'xyz@some.com', phone:'67354829'},
+    {name:'sam',email:'sam@some.com', phone:'34257729'},
+    {name:'joy',email:'joy@some.com', phone:'60238729'}
+  ];
+
+
+  //nested for loop
+  title9 = 'nested for loop'
+  users1=[
+      {name:'isha',email:'isha@some.com', phone:'27238729', social:['facebook','insta', 'snap']},
+      {name:'abc',email:'abc@some.com', phone:'73782929', social:['insta', 'snap']},
+      {name:'xyz',email:'xyz@some.com', phone:'67354829', social:['facebook', 'snap']},
+      {name:'sam',email:'sam@some.com', phone:'34257729', social:['facebook','insta', ]},
+      {name:'joy',email:'joy@some.com', phone:'60238729', social:['facebook','insta', 'snap', 'linkedin']}
+  ];
+
+  //style binding
+  title10 = 'style binding';
+  color2 = "orange"
+  bgcolor="red"
+
+  updateColor()
+  {
+    this.color2="pink"
+    this.bgcolor="skyblue"
+  }
+
+  //heading
+  title11 = 'header component'
 
   //forms
   title12 = 'basic forms.!'
