a25a8da0931d19cb0b69457bb96fccfcaf5e8612
bhavisha550 <bhavishamoghe@gmail.com>
2024-07-05 10:49:49 +0530

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { PatientModule } from "./patient/patient.module"
 import { AppComponent } from "./app.component";
-
+import {FormsModule} from '@angular/forms';
 @NgModule({
     declarations: [
         AppComponent,
     ],
     imports: [
         BrowserModule,
-        PatientModule
+        PatientModule,
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
 })
-export class AppModule{ }
+export class AppModule{ 
+
+}
